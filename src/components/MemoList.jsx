d871ad4004290a68063773188d32c47ff6b81adc
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-01-16 16:55:00 +0900

    Extract Component

---
+++ src/components/MemoList.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function MemoList() {
+  return (
+    <View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2021.01.16 10:00</Text>
+        </View>
+        <View>
+          <Text>x</Text>
+        </View>
+      </View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItemDate}>2021.01.16 10:00</Text>
+        </View>
+        <View>
+          <Text>x</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0, 0.15)',
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+});
