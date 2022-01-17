7b3d0ce4a664014913bab870e81da273112daecb
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-01-17 10:41:13 +0900

    Create LogInScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+// import LogInScreen from './src/screens/LogInScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
-    <MemoEditScreen />
+    <SignUpScreen />
   );
 }
