5f46efa9d92e03eea89fd95b279258c63fd4a74a
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-01-20 09:59:26 +0900

    Add Loading and Styling Button

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -9,11 +9,13 @@ import {
 } from 'react-native';
 import Button from '../components/Button';
 import firebase from 'firebase';
+import Loading from '../components/Loading';
 
 export default function LogInScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
@@ -22,12 +24,15 @@ export default function LogInScreen(props) {
           index: 0,
           routes: [{ name: 'MemoList' }],
         });
+      } else {
+        setLoading(false);
       }
     });
     return unsubscribe;
   }, []);
 
   const handlePress = () => {
+    setLoading(true);
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then((userCredential) => {
         const { user } = userCredential;
@@ -39,11 +44,15 @@ export default function LogInScreen(props) {
       })
       .catch((error) => {
         Alert.alert(error.code);
+      })
+      .then(() => {
+        setLoading(false);
       });
   };
 
   return (
     <View style={styles.container}>
+      <Loading isLoading={isLoading} />
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
         <TextInput
